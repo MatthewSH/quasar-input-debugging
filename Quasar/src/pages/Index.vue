d6c00fd96252827ef8255b4fe48ddcc63b7d6830
Matthew Hatcher <mhatcher@quiktrip.com>
2021-12-29 09:43:21 -0600

    Source.

---
+++ Quasar/src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card>
+      <q-input filled dense label="Number" inputmode="numeric" class="q-mb-md" v-model="num"/>
+      <q-input filled dense label="Date" type="date" class="q-mb-md" v-model="date"/>
+      <q-input filled dense label="Time" type="time" v-model="time"/>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref} from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  setup() {
+    return { num: ref(null), date: ref(null), time: ref(null)}
+  }
+})
+</script>
---
+++ Vue/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <input type="number" /><br/>
+  <input type="date"/><br/>
+  <input type="time"/><br/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+input {
+  margin-bottom: 1rem;
+  width: 80vw;
+  font-size: 1rem;
+}
+</style>
